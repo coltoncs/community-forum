166e106984dbba34a5ca9681e61e9375b2f44f27
unknown <csweeney98@gmail.com>
2019-03-26 18:19:31 -0400

    Initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <nav-bar></nav-bar>
+    <router-view class="container"/>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar";
+
+export default {
+  components: {
+    NavBar
+  }
+};
+</script>
+
+
+<style lang="scss">
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to the Community</h1>
+      <p class="lead">Let's get this bread</p>
+      <hr class="my-4">
+      <p>Managed by pizdetz</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Sign up</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'home',
+}
+</script>
